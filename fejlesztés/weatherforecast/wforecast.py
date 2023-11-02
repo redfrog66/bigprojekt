1c01b3c27ed09db7d1f876229c62081c39b4d7d7
Gellén <gellenrebeka@gmail.com>
2023-11-02 14:19:13 +0100

    Új forrás az adatokhoz

--- fejlesztés/weatherforecast/wforecast.py
+++ fejlesztés/weatherforecast/wforecast.py
@@ -2,26 +2,35 @@
 # Egy API segítségével fogunk időjárás adatokat gyűjteni.
 # Az adatnyerés ehhez hasonló módon fog történni: https://github.com/TechnoLordx/SZFM_Calendar_/blob/master/Source/calendar/weather_script.js
 
-import requests
+import urllib.request
+import sys
 import json
-# Egy Python könyvtár, amely lehetővé teszi a HTTP kérések küldését.
 
-api_key = "your_api_key" #TODO: API kulcs megadása
-city = "Budapest" #TODO: Város megadhatóvá tétele
-url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}"
+api_key = "YOUR_API_KEY" #TODO API KEY
 
-response = requests.get(url)
-# A GET kérés elküldése a megadott URL-re.
-# A válasz tartalmazza a válasz kódját, a fejlécet és a válasz tartalmát.
-data = response.json()
-# A válasz tartalmának JSON formátumra alakítása.
+try: 
+  url = f"https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Hungary/last30days?unitGroup=metric&key={api_key}&contentType=json"
+  ResultBytes = urllib.request.urlopen(url)
+  # TODO - API KEY beillesztése
+  
+  # adatok beolvasása
+  data = json.load(ResultBytes)
+        
+except urllib.error.HTTPError  as e:
+  ErrorInfo= e.read().decode() 
+  print('Error code: ', e.code, ErrorInfo)
+  sys.exit()
+except  urllib.error.URLError as e:
+  ErrorInfo= e.read().decode() 
+  print('Error code: ', e.code,ErrorInfo)
+  sys.exit()
 
 
 # 2. Adatfeldolgozás, adattisztítás
 #releváns adatok kinyerése a válaszból
-temperature = data["main"]["temp"]
-humidity = data["main"]["humidity"]
-pressure = data["main"]["pressure"]
+temperature = data["days"][0]["temp"]
+humidity = data["days"][0]["humidity"]
+pressure = data["days"][0]["pressure"]
 
 
 from sklearn.preprocessing import StandardScaler
@@ -59,3 +68,4 @@ scaler = StandardScaler()
 X_scaled = scaler.fit_transform(X) 
 
 X_train, X_test, y_train, y_test = train_test_split(X_scaled, y, test_size=0.2, random_state=42)
+
