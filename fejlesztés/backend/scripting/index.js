ad278b4b5be825946799aee9454eff4360985553
Gellén <gellenrebeka@gmail.com>
2023-11-16 12:15:14 +0100

    főoldal fetch, adatnyerés változóban tárolt városról

--- fejlesztés/backend/scripting/index.js
+++ fejlesztés/backend/scripting/index.js
@@ -1,1 +1,62 @@
 // Random város kereső
+
+// run: node index.js
+
+// Első lépés: Változóban tárolt város alapján keresés
+let teszt_varos = "Budapest"; // változóban tárolt város
+
+let api_key = "JG5A6TC3EWVAZC5W6P3JZAUGR" 
+
+let url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${teszt_varos}?unitGroup=metric&key=${api_key}&contentType=json`;
+// fetch, node.js-ben, hogy terminálon tudjam tesztelni a kódot
+import('node-fetch').then(module => { // fetch, node.js-ben, hogy terminálon tudjam tesztelni a kódot
+  module.default(url)
+    .then(response => response.json()) 
+    .then(data => {
+      console.log(data);
+    })
+    .catch(error => {
+      console.log('Error:', error);
+    });
+});
+
+// Az összes adat, amit a városról meg tudunk szerezni egy napra:
+/* 
+      datetime: '2023-11-16',
+      datetimeEpoch: 1700089200,
+      tempmax: 12.8,
+      tempmin: 6,
+      temp: 9.6,
+      feelslikemax: 12.8,
+      feelslikemin: 3.3,
+      feelslike: 8.6,
+      dew: 5,
+      humidity: 73.6,
+      precip: 2.7,
+      precipprob: 71.4,
+      precipcover: 12.5,
+      preciptype: [Array],
+      snow: 0,
+      snowdepth: 0,
+      windgust: 28.1,
+      windspeed: 25.9,
+      winddir: 258.1,
+      pressure: 1017,
+      cloudcover: 58.6,
+      visibility: 17.6,
+      solarradiation: 73.3,
+      solarenergy: 6.3,
+      uvindex: 4,
+      severerisk: 10,
+      sunrise: '06:49:30',
+      sunriseEpoch: 1700113770,
+      sunset: '16:07:00',
+      sunsetEpoch: 1700147220,
+      moonphase: 0.11,
+      conditions: 'Rain, Partially cloudy',
+      description: 'Becoming cloudy in the afternoon with late afternoon rain.',
+      icon: 'rain',
+      stations: [Array],
+      source: 'comb',
+      hours: [Array]
+*/
