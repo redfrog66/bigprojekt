eecf40c11dca3f1020d691a7cec64509e9a41c94
Ádám Bukta <90989331+AdamBukta@users.noreply.github.com>
2023-11-16 13:21:06 +0100

    alap converter megírása random számokra (converter.js)

--- fejlesztés/backend/scripting/converter.js
+++ fejlesztés/backend/scripting/converter.js
@@ -1,3 +1,21 @@
-// Átváltó °C -> °F
+// Random számok generálása -100-tól 100-ig
+let randomFahrenheitValue = Math.floor(Math.random() * 201) - 100;
+let randomCelsiusValue = Math.floor(Math.random() * 201) - 100;
 
-// Átváltó °F -> °C
+// °F -> °C képlet
+function fahrenheitToCelsius(fahrenheit) {
+    return (fahrenheit - 32) * (5 / 9);
+}
+
+// °C -> °F képlet
+function celsiusToFahrenheit(celsius) {
+    return (celsius * 9 / 5) + 32;
+}
+
+// °F -> °C random számra
+let convertedCelsius = fahrenheitToCelsius(randomFahrenheitValue);
+console.log(`${randomFahrenheitValue} Fahrenheit is ${convertedCelsius.toFixed(2)} Celsius`);
+
+// °C -> °F random számra
+let convertedFahrenheit = celsiusToFahrenheit(randomCelsiusValue);
+console.log(`${randomCelsiusValue} Celsius is ${convertedFahrenheit.toFixed(2)} Fahrenheit`);
