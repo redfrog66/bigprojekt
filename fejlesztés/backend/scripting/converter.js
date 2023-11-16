73f829232b74848e2deadb43ff73b300d0310aac
Ádám Bukta <90989331+AdamBukta@users.noreply.github.com>
2023-11-16 13:22:11 +0100

    converter megírása bekért számokkal (converter.js)

--- fejlesztés/backend/scripting/converter.js
+++ fejlesztés/backend/scripting/converter.js
@@ -1,21 +1,38 @@
-// Random számok generálása -100-tól 100-ig
-let randomFahrenheitValue = Math.floor(Math.random() * 201) - 100;
-let randomCelsiusValue = Math.floor(Math.random() * 201) - 100;
+const readline = require('readline');
 
-// °F -> °C képlet
-function fahrenheitToCelsius(fahrenheit) {
-    return (fahrenheit - 32) * (5 / 9);
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+function getValue(prompt) {
+  return new Promise((resolve) => {
+    rl.question(prompt, (answer) => {
+      resolve(answer);
+    });
+  });
 }
 
-// °C -> °F képlet
 function celsiusToFahrenheit(celsius) {
-    return (celsius * 9 / 5) + 32;
+  return (celsius * 9 / 5) + 32;
 }
 
-// °F -> °C random számra
-let convertedCelsius = fahrenheitToCelsius(randomFahrenheitValue);
-console.log(`${randomFahrenheitValue} Fahrenheit is ${convertedCelsius.toFixed(2)} Celsius`);
+async function main() {
+  var celsiusInput = await getValue('Adjon meg egy hőmérsékleti értéket Celsiusban tizedes potntosságig (pl.: 16 vagy 34.7): ');
+  var celsiusValue = parseFloat(celsiusInput);
 
-// °C -> °F random számra
-let convertedFahrenheit = celsiusToFahrenheit(randomCelsiusValue);
-console.log(`${randomCelsiusValue} Celsius is ${convertedFahrenheit.toFixed(2)} Fahrenheit`);
+  if (isNaN(celsiusValue)) {
+    console.log('Helytelen érték.');
+    rl.close();
+    return;
+  }
+
+  var fahrenheitValue = celsiusToFahrenheit(celsiusValue);
+  
+  console.log(`${celsiusValue.toFixed(1)} Celsius is ${fahrenheitValue.toFixed(1)} Fahrenheit`);
+  console.log(`${fahrenheitValue.toFixed(1)} Fahrenheit is ${celsiusValue.toFixed(1)} Celsius`);
+
+  rl.close();
+}
+
+main();
