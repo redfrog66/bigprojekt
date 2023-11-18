9a50cfc5455b28a57775146585d6df6beeb9ecaa
Gellén <gellenrebeka@gmail.com>
2023-11-18 11:28:44 +0100

    archivumhoz tartozó js feladatainak rögzítése

--- fejlesztés/backend/scripting/archive.js
+++ fejlesztés/backend/scripting/archive.js
@@ -1,1 +1,13 @@
 // Visszamenő adatok hozzáférése
+
+// Ezen a felületen a visszamenő adatokat lehet lekérdezni. Olyan API-val dolgozunk, ahol 30 napig visszamenőleg tudjuk lekérni az adatokat.
+// Ennél régebbihez már nem férünk hozzá, ugyanis előfizetéshez kötött a szolgáltatás. Egy nagyobb szabású projekt esetén érdemes lehet előfizetni.
+
+// Cél: adott nap visszamenőlegi adatok lekérdezése, adott helyre
+// 1. node.js segítségével terminálra tudjuk kiírni a lekérdezett adatokat
+/* 
+    1.1 Adott városra - azaz az index.js már működő kódjának átvétele
+    1.2 Adott napra - azaz a dátumokat is be kell vinni
+    1.3 billentyűzetről beolvasott adatokat kell feldolgozni
+*/
+// 2. Felülettel összeköthetővé alakítjuk a kódot
